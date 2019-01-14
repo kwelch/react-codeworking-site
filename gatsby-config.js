e436f6c6e5580019c3b0539f93d8e33db0440379
Kyle Welch <kwelch0626@gmail.com>
2019-01-13 19:34:25 -0600

    initial changes to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `React CodeWorking`,
+    description: `React CodeWorking is a monthly co-working event for React developers in Nashville.`,
+    author: `@kwelch`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -31,4 +31,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
   ],
-}
+};
