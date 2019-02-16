fda8a65a9b12231d3dc499eb87b1afea2b569c69
Kyle Welch <kwelch0626@gmail.com>
2019-02-15 23:50:10 -0600

    404 page update

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>¯\_(ツ)_/¯</h1>
+    <p>This route may not be found, but we hope to find you soon.</p>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
