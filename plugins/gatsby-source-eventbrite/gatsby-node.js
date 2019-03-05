022f2f65c39df1730187aa7b5deb0f574e1d91b3
marcosparagna-eb <marco.sparagna@eventbrite.com>
2019-03-05 16:28:16 +0100

    es6-ified eb plugin

--- plugins/gatsby-source-eventbrite/gatsby-node.js
+++ plugins/gatsby-source-eventbrite/gatsby-node.js
@@ -1,11 +1,12 @@
 const eventbrite = require('eventbrite').default;
 
-exports.sourceNodes = function(
+exports.sourceNodes = async function(
   { actions: { createNode }, createNodeId, createContentDigest },
   { organizerId, token, verbose = true }
 ) {
   const processEvent = (event) => {
-    return Object.assign({}, event, {
+    return {
+      ...event,
       id: createNodeId(`eventbrite-event-${event.id}`),
       parent: null,
       children: [],
@@ -14,23 +15,23 @@ exports.sourceNodes = function(
         content: JSON.stringify(event),
         contentDigest: createContentDigest(event),
       },
-    });
+    };
   };
   if (!token) {
     throw new Error("Missing Eventbrite OAuth token");
   }
   const sdk = eventbrite({ token });
+  try {
+    const { events } = await sdk.request(
+      `/organizers/${organizerId}/events/?only_public=on&expand=venue`
+    );
 
-  return sdk
-    .request(`/organizers/${organizerId}/events/?only_public=on&expand=venue`)
-    .then(({ events }) => {
-      events.forEach((event) => {
-        const eventNode = processEvent(event);
+    events.forEach((event) => {
+      const eventNode = processEvent(event);
 
-        createNode(eventNode);
-      });
-    })
-    .catch((err) => {
-      console.error('EB Fetch fail:', err);
+      createNode(eventNode);
     });
+  } catch (err) {
+    console.error('EB Fetch fail:', err);
+  }
 };
