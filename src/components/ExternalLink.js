fd4d11dc48017edb3491295cf4e530fb851a195c
Chance Strickland <chance.strickland@gmail.com>
2019-03-05 09:26:55 -0600

    rm eslint ignore

--- src/components/ExternalLink.js
+++ src/components/ExternalLink.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default function ExternalLink({ rel = '', children, ...props }) {
-  // eslint-disable-next-line jsx-a11y/anchor-has-content
   return (
     <a
       target="_blank"
