831be4ccacdb13af08b72238a6d011fb4f62e7ec
Kyle Welch <kwelch0626@gmail.com>
2019-03-04 22:48:24 -0600

    add code of conduct page

---
+++ src/pages/coc.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import { withGutters, darkAccent, styleLinks } from '../components/styles';
+
+export default function HomePage(props) {
+  return (
+    <Layout>
+      <div className="page-container">
+        <SEO title="Code of Conduct" />
+        <div css={[withGutters, { marginBottom: '2rem' }]}>
+          <h1 css={{ marginBottom: '1rem' }}>Code of conduct</h1>
+          <p css={[styleLinks({ color: darkAccent })]}>
+            React CodeWorking follows the{' '}
+            <a href="http://confcodeofconduct.com/">
+              conference code of conduct
+            </a>
+            . Violations of the code of conduct will result in being asked to
+            leave from the event and banning from future events.
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+}
