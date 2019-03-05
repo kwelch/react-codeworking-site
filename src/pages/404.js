c47c07dbb1675a2daab7dbcad65b268433477b80
Kyle Welch <kwelch0626@gmail.com>
2019-03-04 21:30:30 -0600

    clean up 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import { withGutters } from '../components/styles';
 
 export default function NotFoundPage() {
   return (
     <Layout>
       <SEO title="404: Not found" />
-      <div className="page-container">
-        <h2>¯\_(ツ)\_/¯</h2>
+      <div css={[withGutters]}>
+        <h1 css={{ margin: '1.5rem 0' }}>¯\_(ツ)\_/¯</h1>
 
-        <p>This route may not be found, but we hope to find you soon.</p>
+        <h2 css={{ margin: '1.2rem 0' }}>
+          This route may not be found, but we hopefully we will find you at the
+          next event.
+        </h2>
       </div>
     </Layout>
   );
